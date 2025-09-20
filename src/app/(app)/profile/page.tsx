b727fa377952ae3aefea053e83e6615945eeb898
Khalandar Thameem <khalandarthameem97@gmail.com>
2025-09-20 08:32:51 +0000

    Try fixing this error: `Runtime Error: ReferenceError: format is not def

--- src/app/(app)/profile/page.tsx
+++ src/app/(app)/profile/page.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
+import { format } from "date-fns";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
